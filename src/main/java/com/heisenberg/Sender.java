264b3cf6f587838c1e90d1e17a0150a0b2a06f8d
Robin Calamatta <calamatta89@gmail.com>
2023-12-25 19:40:21 -0500

    added the sender class and logic

---
+++ src/main/java/com/heisenberg/Sender.java
@@ -0,0 +1,22 @@
+package com.heisenberg;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Sender {
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+
+        try (Connection connection = factory.newConnection()){
+            Channel channel = connection.createChannel();
+            channel.queueDeclare("hello-world", false, false, false, null);
+            String message = "is this the matrix";
+            channel.basicPublish("", "hello-world", false, null, message.getBytes());
+            System.out.println("!!! Message has been sent!");
+        }
+    }
+}
